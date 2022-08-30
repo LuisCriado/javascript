97ed462aa129fa531082720754130151f1793ed8
Pablo Gutierrez <pablo.guty.2008@gmail.com>
2022-08-30 17:57:13 -0300

    nuevo repo js

---
+++ assets/javascript/comision-entrega1.js
@@ -0,0 +1,29 @@
+// calculadora porcentaje de comision 
+
+debugger
+
+//Saludo bienvenida 
+
+let nombreCompleto = prompt ("Ingrese su nombre completo, por favor:");
+alert ("Bienvenido a la calculadora de porcentaje por comisión, " + nombreCompleto);
+
+//variables
+
+
+
+//informacion de usuario
+
+function datosIngresados(){
+    let numero = parseFloat(prompt("Ingrese el monto total:"));
+    let porcentaje= parseFloat(prompt ("Ingrese el porcentaje:"));
+    let resultado = (numero * porcentaje / 100);
+}
+
+datosIngresados();
+
+let numeroDeVentas= prompt("Número de ventas en el dia:");
+let comisionPorVenta= 10;
+
+do {
+    if()
+}
---
+++ assets/javascript/entregaComplementaria.js
@@ -0,0 +1,21 @@
+//Solución problema (pedir repeticiones de un texto y que alerte en pantalla)
+
+/* let repeticiones = parseInt(prompt("Ingresar repeticones"));
+let texto = prompt ("Ingresar texto a repetir");
+
+debugger
+
+for (let i=0; i<repeticiones; i++) {
+
+    console.log(texto);
+
+} */
+
+//Solución problema (multiplicacion de números)
+/* 
+let numero1 = parseInt(prompt("Ingresar número"));
+let numero2 = parseInt(prompt("Ingrese número a multiplicar"));
+
+let resultado = numero1 * numero2;
+
+alert(resultado); */
